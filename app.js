b4bd97160e2fec0c04126d8964cfa6d9fbe2895b
Azamorn <azamorn@gmail.com>
2021-05-17 15:22:04 +0200

    Allow bot to be enabled/disabled and increased interval to 15 mins

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 const fs = require('fs'), path = require('path');
 const bot = new TelegramBot(process.env.TOKEN, {polling: true});
-const command = '/startpricebot';
-var chatId = null;
+const start_command = '/startpricebot',
+stop_command = '/stoppricebot';
+var chatId = null, intervalTimer = null;
 
 function getPriceJson(){
     let json = JSON.parse(fs.readFileSync(path.resolve('../','discord-bot','latest-price.json')).toString());
@@ -14,15 +15,25 @@ function getPriceJson(){
 
 bot.on('message', (msg) => {
     if(chatId == null){
-        if(msg.text.toString().toLowerCase().includes(command)){
-            console.log("Price Bot Enabled")
-            chatId = msg.chat.id;
-            setInterval(() => {
-                let price_json = getPriceJson();
-                if(typeof price_json.latest_price != 'undefined'){
-                    bot.sendMessage(chatId, `Current Price: $${price_json.latest_price} ${price_json.price_movement}`);
-                }
-            }, 300000);
-        }
+        chatId = msg.chat.id;
+    }
+    if(msg.text.toString().toLowerCase().includes(start_command) && intervalTimer == null){
+        bot.sendMessage(chatId, `Price Watching: Enabled`);
+        console.log(`Price Watching: Enabled`)
+        watchTiddiesPrice();
+    }
+    else if(msg.text.toString().toLowerCase().includes(stop_command) && intervalTimer != null){
+        bot.sendMessage(chatId, `Price Watching: Disabled`);
+        console.log(`Price Watching: Disabled`)
+        clearInterval(intervalTimer)
     }
 });
+
+function watchTiddiesPrice(){
+    intervalTimer = setInterval(() => {
+        let price_json = getPriceJson();
+        if(typeof price_json.latest_price != 'undefined'){
+            bot.sendMessage(chatId, `Current Price: $${price_json.latest_price} ${price_json.price_movement}`);
+        }
+    }, 300000);
+}
